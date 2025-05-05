00167d41eb312d01a9ebfcd4c5614c5eb38f9b84
navanath-latake <navanath.latake01@gmail.com>
2025-05-05 10:04:34 +0530

    variable.py

---
+++ 00Variables.py
@@ -0,0 +1,90 @@
+'''
+Variables in Python:-
+-A python variable is a reserved memory location to store values.
+-(i.e a variable is a python program gives data to the computer for processing)
+-In short,Variables are containers for storing data values.
+  
+Variable म्हणजे: प्रोग्रॅममध्ये डेटा साठवण्यासाठी वापरण्यात येणारी कंटेनर (पात्र).
+
+
+
+
+Rules for Python variables:
+
+1]A variable name must start with a letter or the underscore character
+2]A variable name cannot start with a number
+3]A variable name can only contain alpha-numeric characters and underscores (A-z, 0-9, and _ )
+4]Variable names are case-sensitive (age, Age and AGE are three different variables)
+5]The reserved words(keywords)cannot be used naming the variables.
+
+
+'''
+# a = 10
+# print(a)
+
+
+# # myvar = "renuka"   #start with letter
+# print(myvar)
+
+
+# my_var = "Anidha"   #allowed _ in between two words...words are seperated by _(Snake_Case)
+# print(my_var)
+
+# _my_var = "Priyanka"  #allowed _ start with letter
+# print(_my_var)
+
+# myvar2 = "John"   
+# print(myvar2)
+
+# myVar = "Sonali"   #(Camel Case)
+# print(myVar)
+
+# MyVar = "Bhagyashree" #(Pascal Case)
+# print(MyVar)
+
+# MYVAR = "Sonam"
+# print(MYVAR)
+
+# 2myvar = "Pratik"
+# print(myvar2)
+
+# my-var = "John"
+# print(my-var)
+
+
+# my var = "John"
+# print(my var)
+
+# True = "Dhanu"
+# print(True)
+
+# Num = 100
+# # print("Before declare:",Num)
+
+# Num = 101
+# print("After declare:",Num)
+
+# a = b = c = 10
+# print(a,b,c)
+
+# a,b,c = 12,23.67, "look"
+# print(a,b,c)
+
+
+#do by yourself 10 examples
+
+
+
+
+
+
+
+
+
+
+
+
+
+# shri=10
+# print(shri)
+
